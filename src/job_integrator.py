53114bce947dce03a3d983f4dd9584ab56078b9f
AnnTsai <48993103+anntsai2356@users.noreply.github.com>
2023-03-23 00:21:46 +0800

    adds jobs integration (#11)
    * modify importing method for JobInfo class
    
    * add JobIntegrater class and function of importing job site data
    
    * add grouping data of job sites
    
    * add build function to create integrated file
    
    * add ignored folder (temp, data)
    
    * keep data folder
    
    * fix test code in JobIntegrater
    
    * ignore data folder
    
    * modify method to get response in JobIntegrator for replacing read/write file
    
    * extract file path for using on CLI option

---
+++ src/job_integrator.py
@@ -0,0 +1,96 @@
+import csv
+import json
+import pathlib
+from job_info import JobInfo
+from site_types import SiteType
+
+base_directory = pathlib.Path(__file__).parents[1]
+
+
+class JobIntegrator:
+    jobs: dict[SiteType, list[JobInfo]] = {}
+
+    def __init__(self) -> None:
+        self.jobs = {}
+
+    def add(self, site_type: SiteType, job_list: list) -> None:
+        if job_list == {}:
+            return
+
+        if self.jobs != {}:
+            assert (
+                site_type not in self.jobs.keys()
+            ), "WARN: site type has exist in jobs dictionary."
+
+        self.jobs[site_type.name] = job_list
+
+    def _groupData(self) -> dict:
+        jobs_group = {}
+
+        if not self.jobs:
+            return jobs_group
+
+        for platform_name, platform_data in self.jobs.items():
+            for item in platform_data:
+                platform = {}
+                job_unique_key = ""
+
+                job_unique_key = item.title + "_" + item.company
+                platform = {
+                    "name": platform_name,
+                    "updated_time": item.updated_time,
+                    "url": item.url,
+                }
+
+                if job_unique_key in jobs_group:
+                    jobs_group[job_unique_key]["platforms"][platform_name] = platform
+                else:
+                    platforms = {}
+                    job = {}
+                    platforms[platform_name] = json.dumps(platform)
+                    job = {
+                        "title": item.title,
+                        "company": item.company,
+                        "location": item.location,
+                        "platforms": platforms,
+                    }
+                    jobs_group[job_unique_key] = job
+
+        return jobs_group
+
+    def combineAndExport(self, file_path) -> None:
+        fields = [
+            "title",
+            "company",
+            "location",
+            "platforms",
+        ]
+        jobs_group = self._groupData()
+
+        with open(file_path, "w", encoding="utf-8", newline="") as f:
+            csvfile = csv.DictWriter(f, fieldnames=fields)
+            csvfile.writeheader()
+            csvfile.writerows(jobs_group.values())
+
+
+if __name__ == "__main__":
+    from jobs_request import RequestHelperHandle
+
+    # for test
+    sites = [
+        # SiteType.CAKERESUME,
+        # SiteType.OZF,
+        SiteType.YOURATOR,
+    ]
+    file_path = pathlib.Path(base_directory).joinpath("data", "jobs.csv")
+    #---
+
+    integrator = JobIntegrator()
+    for site_type in sites:
+        jobs: list[JobInfo] = []
+        request_helper = RequestHelperHandle.get(site_type)
+        jobs = request_helper.getJobsList()
+
+        integrator.add(site_type, jobs)
+
+    integrator.combineAndExport(file_path)
