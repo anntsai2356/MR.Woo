76ce7b9d8ee054277b82eddb6e004d7543698de0
anntsai2356 <hikarunost@gmail.com>
2023-03-29 18:49:43 +0800

    optimize integrator and add upsert method to update history data

--- src/job_integrator.py
+++ src/job_integrator.py
@@ -1,76 +1,159 @@
-import csv
-import json
 import pathlib
+from datetime import datetime
 from job_info import JobInfo
+from preference_types import PreferenceType
 from site_types import SiteType
+from utils.file_helper import FileHelper
 
 base_directory = pathlib.Path(__file__).parents[1]
 
 
 class JobIntegrator:
-    jobs: dict[SiteType, list[JobInfo]] = {}
+    fields = [
+        "title",
+        "company",
+        "location",
+        "updated_time",
+        "url",
+        "platform",
+        "preference",
+    ]
+    jobs = []
 
     def __init__(self) -> None:
-        self.jobs = {}
+        self.jobs = []
 
-    def add(self, site_type: SiteType, job_list: list) -> None:
-        if job_list == {}:
+    def add(self, site_type: SiteType, site_jobs: list) -> None:
+        if site_jobs == []:
             return
 
-        if self.jobs != {}:
-            assert (
-                site_type not in self.jobs.keys()
-            ), "WARN: site type has exist in jobs dictionary."
+        for site_job in site_jobs:
+            item = {}
+            item = {
+                "title": site_job.title,
+                "company": site_job.company,
+                "location": site_job.location,
+                "updated_time": datetime.fromtimestamp(site_job.updated_time),
+                "url": site_job.url,
+                "platform": site_type.name,
+                "preference": PreferenceType.UNASSIGNED.value,
+            }
+            self.jobs.append(item)
 
-        self.jobs[site_type.name] = job_list
+    def _group(self, data: list) -> dict:
+        job_group = {}
 
-    def _groupData(self) -> dict:
-        jobs_group = {}
+        if data == []:
+            return job_group
 
-        if not self.jobs:
-            return jobs_group
+        for job in data:
+            platform = {}
 
-        for platform_name, platform_data in self.jobs.items():
-            for item in platform_data:
-                platform = {}
-                job_unique_key = ""
+            job_unique_key = ""
+            job_unique_key = job["title"] + "_" + job["company"]
 
-                job_unique_key = item.title + "_" + item.company
-                platform = {
-                    "name": platform_name,
-                    "updated_time": item.updated_time,
-                    "url": item.url,
+            # ozf has many url at the same job, so replace it, just save one of them
+            platform = {
+                "name": job["platform"],
+                "updated_time": job["updated_time"],
+                "url": job["url"],
+            }
+
+            if job_unique_key in job_group:
+                job_group[job_unique_key]["platforms"][job["platform"]] = platform
+            else:
+                platforms = {}
+                item = {}
+                platforms[job["platform"]] = platform
+                item = {
+                    "title": job["title"],
+                    "company": job["company"],
+                    "location": job["location"],
+                    "preference": job["preference"],
+                    "platforms": platforms,
                 }
+                job_group[job_unique_key] = item
 
-                if job_unique_key in jobs_group:
-                    jobs_group[job_unique_key]["platforms"][platform_name] = platform
-                else:
-                    platforms = {}
-                    job = {}
-                    platforms[platform_name] = json.dumps(platform)
-                    job = {
-                        "title": item.title,
-                        "company": item.company,
-                        "location": item.location,
-                        "platforms": platforms,
-                    }
-                    jobs_group[job_unique_key] = job
+        return job_group
 
-        return jobs_group
+    """
+    unique_key: group by title and company for searching
+    from_data: new getting data
+    to_data: history data
 
-    def combineAndExport(self, file_path) -> None:
-        fields = [
-            "title",
-            "company",
-            "location",
-            "platforms",
-        ]
-        jobs_group = self._groupData()
+    update history data with new data
+    """
 
-        with open(file_path, "w", encoding="utf-8", newline="") as f:
-            csvfile = csv.DictWriter(f, fieldnames=fields)
-            csvfile.writeheader()
-            csvfile.writerows(jobs_group.values())
+    def _updateData(self, unique_key, from_data: dict, to_data: dict):
+        platform = {
+            "name": from_data["platform"],
+            "updated_time": from_data["updated_time"],
+            "url": from_data["url"],
+        }
+        to_data[unique_key]["platforms"][from_data["platform"]] = platform
+
+    """
+    unique_key: group by title and company for searching
+    from_data: new getting data
+    to_data: history data
+
+    insert new data into history data
+    """
+
+    def _insertData(self, unique_key, from_data: dict, to_data: dict):
+        platforms = {}
+        job = {}
+        platform = {
+            "name": from_data["platform"],
+            "updated_time": from_data["updated_time"],
+            "url": from_data["url"],
+        }
+        platforms[from_data["platform"]] = platform
+        job = {
+            "title": from_data["title"],
+            "company": from_data["company"],
+            "location": from_data["location"],
+            "preference": from_data["preference"],
+            "platforms": platforms,
+        }
+        to_data[unique_key] = job
+
+    def _removeGrouping(self, data_group) -> list:
+        result = []
+
+        for data in data_group.values():
+            for platform_info in data["platforms"].values():
+                item = {}
+                item = {
+                    "title": data["title"],
+                    "company": data["company"],
+                    "location": data["location"],
+                    "updated_time": platform_info["updated_time"],
+                    "url": platform_info["url"],
+                    "platform": platform_info["name"],
+                    "preference": data["preference"],
+                }
+                result.append(item)
+
+        return result
+
+    def upsert(self, to_file) -> None:
+        to_data = FileHelper.importDictData(to_file, self.fields)
+        if to_data == []:
+            return
+
+        to_data_group = self._group(to_data)
+        for job in self.jobs:
+            unique_key = job["title"] + "_" + job["company"]
+            if unique_key in to_data_group.keys():
+                self._updateData(unique_key, job, to_data_group)
+            else:
+                self._insertData(unique_key, job, to_data_group)
+
+        self.jobs = self._removeGrouping(to_data_group)
+
+    def export(self, file_path) -> None:
+        FileHelper.exportData(file_path, self.fields, self.jobs, True)
 
 
 if __name__ == "__main__":
@@ -78,12 +161,14 @@ if __name__ == "__main__":
 
     # for test
     sites = [
-        # SiteType.CAKERESUME,
-        # SiteType.OZF,
+        SiteType.CAKERESUME,
+        SiteType.OZF,
         SiteType.YOURATOR,
     ]
-    file_path = pathlib.Path(base_directory).joinpath("data", "jobs.csv")
-    #---
+    file_name = f'jobs_{datetime.now().strftime("%Y-%m-%d")}.csv'
+    file_path = pathlib.Path(base_directory).joinpath("data", file_name)
+    to_file_path = pathlib.Path(base_directory).joinpath("data", "jobs.csv")
+    # ---
 
     integrator = JobIntegrator()
     for site_type in sites:
@@ -93,4 +178,9 @@ if __name__ == "__main__":
 
         integrator.add(site_type, jobs)
 
-    integrator.combineAndExport(file_path)
+    # deal with new result at now
+    integrator.export(file_path)
+
+    # deal with new and history result
+    integrator.upsert(to_file_path)
+    integrator.export(to_file_path)
