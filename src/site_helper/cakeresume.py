e4d92a605b7b2cc68e87869c4a76670145e4ca12
AnnTsai <48993103+anntsai2356@users.noreply.github.com>
2023-03-31 18:10:01 +0800

    obtains app token for Algolia in Cakeresume (#24)
    * modify "platform" to "site"
    
    * add method to get algolia app id and api key
    
    * modify algolia function position
    
    * remove todo comment

--- src/site_helper/cakeresume.py
+++ src/site_helper/cakeresume.py
@@ -1,6 +1,9 @@
 from site_helper.base import AbstractSiteHelper as _AbstractSiteHelper, ParserHelper as _ParserHelper
 from requests import post as _requestPost, Response as _Response
 from urllib.parse import urlencode as _urlEncode
+import requests
+import json
+import re
 
 from job_info import JobInfo
 from site_types import SiteType
@@ -18,12 +21,14 @@ class CakeresumeHelper(_AbstractSiteHelper):
         self._current_page = 0
 
     def _doRequestJobs(self, *args) -> _Response:
+        search_page_url = "https://www.cakeresume.com/jobs"
+        algolia = self.getCakeresumeToken(search_page_url)
+
         URL = "https://966rg9m3ek-dsn.algolia.net/1/indexes/*/queries?"
-        # TODO: handle x-algolia-api-key
         PARAMS = {
             "x-algolia-agent": "Algolia for JavaScript (4.14.2); Browser (lite); instantsearch.js (4.49.1); react (18.2.0); react-instantsearch (6.38.1); react-instantsearch-hooks (6.38.1); JS Helper (3.11.1)",
-            "x-algolia-api-key": "ZThkNmExMWRiZjQ4ZGJlZWU4YTc1MTEyN2U2Y2ViZTUxMWM1MTdlODM4YWE2MWYzYThhZWI1NzZiM2Y5ZTI5N3ZhbGlkVW50aWw9MTY4MDYyMjk1MSZyZXN0cmljdEluZGljZXM9Sm9iJTJDSm9iX29yZGVyX2J5X2NvbnRlbnRfdXBkYXRlZF9hdCUyQ0pvYl9wbGF5Z3JvdW5kJTJDUGFnZSUyQ1BhZ2Vfb3JkZXJfYnlfY29udGVudF91cGRhdGVkX2F0JmZpbHRlcnM9YWFzbV9zdGF0ZSUzQSslMjJjcmVhdGVkJTIyK0FORCtub2luZGV4JTNBK2ZhbHNlJmhpdHNQZXJQYWdlPTEwJmF0dHJpYnV0ZXNUb1NuaXBwZXQ9JTVCJTIyZGVzY3JpcHRpb25fcGxhaW5fdGV4dCUzQTgwJTIyJTVEJmhpZ2hsaWdodFByZVRhZz0lM0NtYXJrJTNFJmhpZ2hsaWdodFBvc3RUYWc9JTNDJTJGbWFyayUzRQ==",
-            "x-algolia-application-id": "966RG9M3EK",
+            "x-algolia-api-key": algolia["api_key"],
+            "x-algolia-application-id": algolia["app_id"],
         }
         SEARCH_JSON = {
             "requests": [
@@ -86,3 +91,50 @@ class CakeresumeHelper(_AbstractSiteHelper):
 
     def _hasRemainingJobs(self) -> bool:
         return self._current_page < self._total_pages
+
+    def getCakeresumeToken(self, url: str) -> dict:
+        """
+        url : The url is page that use search box.
+
+        There has many keys in cake resume.
+        only key_jobs_and_pages is used to search Job index.
+
+        Ex:
+        {
+            "algolia": {
+                "id": "966RG9M3EK",
+                "key": "NjNhZ...", (length = 620)
+                "key_global_search": "MzA0M2...", (length = 284)
+                "key_query_suggestions": "MDliO...", (length = 396)
+                "key_users": "ZTZiM...", (length = 360)
+                "key_jobs_and_pages": "NDM0O...", (length = 520)
+                "key_featured_pages": "ZGUxY...", (length = 312)
+                "key_featured_jobs": "NmU1M...", (length = 476)
+                "key_published_posts": "ZThhN...", (length = 316)
+                "key_organizations": "OTkwZ...", (length = 252)
+                "key_activities": "MmYwN...", (length = 244)
+                "key_articles": "OThkM...", (length = 292)
+                "key_featured_items_tw_user": "MDgyY...", (length = 356)
+                "key_featured_items_non_tw_user": "ZDM1Y..." (length = 423)
+            }
+        }
+        """
+
+        result = {"app_id": "", "api_key": ""}
+
+        response = requests.get(url)
+        response.encoding = "utf-8"
+        decoded_content = response.content.decode(response.encoding)
+
+        match = re.search('("algolia"?):{(.+?)}', decoded_content).group()
+        match = "{" + match + "}"
+        info = json.loads(match.replace("'", '"'))
+
+        if not info["algolia"]["id"] and not info["algolia"]["key_jobs_and_pages"]:
+            print(f"WARN: Could not find algolia keys in {url}.")
+            return result
+
+        result["app_id"] = info["algolia"]["id"]
+        result["api_key"] = info["algolia"]["key_jobs_and_pages"]
+
+        return result    
