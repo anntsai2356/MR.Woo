7d24ddccd9d4dd21d49401103fec459f4c76161c
anntsai2356 <hikarunost@gmail.com>
2023-04-02 18:47:42 +0800

    remove site_param_helper

--- src/site_param_helper/yourator.py
+++
@@ -1,8 +0,0 @@
-from site_param_helper.base import AbstractSiteParamHelper as _AbstractSiteParamHelper
-
-
-class YouratorParamHelper(_AbstractSiteParamHelper):
-    def __init__(self) -> None:
-        self._param_name_mapping: dict = {
-            "keyword": "term[]",
-        }
